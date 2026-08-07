4d781d01275b50a9574bce23338d3c696db34859
Manthan <manthanrathore2172005@gmail.com>
2026-08-07 20:44:49 +0530

    integrate socket.io for real-time communication with authentication middleware

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,9 @@ import "dotenv/config";
 import http from "http";
 import app from "./app.js";
 import connectToDB from "./src/db/db.js";
+import {Server} from 'socket.io'
+import jwt from "jsonwebtoken"
+
 
 connectToDB()
 
@@ -10,7 +13,42 @@ connectToDB()
 const port = process.env.PORT || 3000;
 
 const server = http.createServer(app);
+const io = new Server(server);
 
+io.use((socket, next) => {   // socket io ke through ek authenticate user hi connect kar paye
+
+    try{
+
+        const token = socket.handshake.auth?.token || socket.handshake.headers.authorization?.split(" ")[1];
+
+        if(!token){
+            return next(new Error("Authentication Error"));
+        }
+
+        const decoded= jwt.verify(token, process.env.JWT_SECRET);
+
+        if(!decoded){
+            return next(new Error("Authentication Error"));
+        }
+
+        socket.user = decoded;
+
+        next();
+
+    } catch(err){
+        next(err);
+    }
+})
+
+
+
+
+io.on('connection', socket => {
+    console.log("a user connected");
+    
+  socket.on('event', data => { /* … */ });
+  socket.on('disconnect', () => { /* … */ });
+});
 
 server.listen(port, () => {
     console.log(`Server is running on port ${port} `); 
